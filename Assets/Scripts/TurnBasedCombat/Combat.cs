b469cac937810d67dcd4d31cb8e43693f9e50026
FIEA\jdejesus <jarory13@gmail.com>
2018-06-09 15:36:11 -0400

    Make attackBTN use acting ally variable instead of stupid priority number. Also make find enemy return the closest enemy to you

--- Assets/Scripts/TurnBasedCombat/Combat.cs
+++ Assets/Scripts/TurnBasedCombat/Combat.cs
@@ -18,12 +18,15 @@ public class Combat : MonoBehaviour
     public float testTickTime;
     Button btn;
     [SerializeField]
-    public Ally ally;
+    public Ally actingAlly;
     public Enemy enemy;
-    public GameObject [] enemies;
-    public GameObject [] allies;
+    public List<GameObject> enemies = new List<GameObject>();
+    public List<GameObject> allies = new List<GameObject>();
     int rng;
-    
+
+    [SerializeField]
+    private float sphereCastRange;
+
 
     private void Start()
     {
@@ -43,63 +46,126 @@ public class Combat : MonoBehaviour
 
         SetUpCanvases();
         // for the attack button
-        enemies = GameObject.FindGameObjectsWithTag ( "Enemy" );
-        allies = GameObject.FindGameObjectsWithTag ( "Player" );
+        GameObject[] tempEnemies;
+        tempEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        for (int i = 0; i < tempEnemies.Length; i++)
+        {
+            enemies.Add(tempEnemies[i]);
+        }
+
+        Ally[] tempAllies;
+
+        tempAllies = GameObject.FindObjectsOfType<Ally>();
         // does this need to be here? is combsat gonna be on the GM permantly or is it gonna swap with a out of combat script?
+
+        for (int i = 0; i < tempAllies.Length; i++)
+        {
+            allies.Add(tempAllies[i].gameObject);
+        }
+
         CombatEnter();
     }
     // this update checks to see if the prioity is -1 if it is then it sets ally to null if its 5 or 4 then it sets it to shiro or binx
-    void Update () {
-        if ( CharacterWithPriority != -1 ) {
+    void Update()
+    {
+        if (CharacterWithPriority != -1)
+        {
             // needs fixing to work with tag of Player this is a temporay fix
-            if ( CharacterWithPriority == 4 ) {
-                ally = GameObject.FindGameObjectWithTag ( "Shiro" ).GetComponent<Shiro> ();
+            if (CharacterWithPriority == 4)
+            {
+                actingAlly = GameObject.FindGameObjectWithTag("Shiro").GetComponent<Shiro>();
                 Cursor.lockState = CursorLockMode.None;
-            } else if ( CharacterWithPriority == 5 ) {
-                ally = GameObject.FindGameObjectWithTag ( "Binx" ).GetComponent<Binx> ();
-                Debug.Log ( "dragons" );
+            }
+            else if (CharacterWithPriority == 5)
+            {
+                actingAlly = GameObject.FindGameObjectWithTag("Binx").GetComponent<Binx>();
+                Debug.Log("dragons");
                 Cursor.lockState = CursorLockMode.None;
 
             }
-        } else if ( CharacterWithPriority == -1 ) {
-            ally = null;
+        }
+        else if (CharacterWithPriority == -1)
+        {
+            actingAlly = null;
             Cursor.lockState = CursorLockMode.Locked;
-        } else {
-            Debug.Log ( "WTF!" );
+        }
+        else
+        {
+            Debug.Log("WTF!");
         }
     }
     //DO NOT CHANGE THIS IT TOOK ME FOREVER TO GET WORKING
-    public void TurnPass () {
-        if ( CharacterWithPriority != -1 ) {
-            if ( CharacterWithPriority == 4 ) {
-                ally.CanAct = false;
-                ally.AttackBar = 0;
-                ally.PassTurn ();
-                ally.DisablePlayerControl ();
+    public void TurnPass()
+    {
+        if (CharacterWithPriority != -1)
+        {
+            if (CharacterWithPriority == 4)
+            {
+                actingAlly.CanAct = false;
+                actingAlly.AttackBar = 0;
+                actingAlly.PassTurn();
+                actingAlly.DisablePlayerControl();
                 return;
-            } else if ( CharacterWithPriority == 5 ) {
-                ally.CanAct = false;
-                ally.AttackBar = 0;
-                ally.PassTurn ();
-                ally.DisablePlayerControl ();
+            }
+            else if (CharacterWithPriority == 5)
+            {
+                actingAlly.CanAct = false;
+                actingAlly.AttackBar = 0;
+                actingAlly.PassTurn();
+                actingAlly.DisablePlayerControl();
                 return;
             }
         }
     }
-    
-        //this find the enemy script
-    private void FindEnemies () {
-        List<GameObject> enemiesS = new List<GameObject> ();
-        Collider [] collisions;
-        collisions = Physics.OverlapSphere ( transform.position, 2.5f );
 
-        for ( int i = 0; i < collisions.Length; i++ ) {
+    //this find the enemy script
+    private Enemy FindEnemies()
+    {
+
+        Enemy enemy = null;
+        Enemy CompareEnemy = null;
+
+        //We'll do an overlap sphere to see what enemies are within range 2.5f should eventually be made into a private serialized variable
+        Collider[] collisions;
+        collisions = Physics.OverlapSphere(actingAlly.gameObject.transform.position, sphereCastRange);
+
+        for (int i = 0; i < collisions.Length; i++)
+        {
             //you can filter here however you want this is just one example
-            if ( collisions [ i ].gameObject.CompareTag ( "Enemy" ) ) {
-                enemiesS.Add ( collisions [ i ].gameObject );
+            if (collisions[i].gameObject.CompareTag("Enemy"))
+            {
+
+                Debug.Log("Enemies found");
+                enemy = collisions[i].gameObject.GetComponent<Enemy>();
+
+                //Set the compare enemy if it's null
+                if (CompareEnemy == null)
+                {
+                    CompareEnemy = enemy;
+                }
+                else
+                {
+
+                    //figure out which enemy is closer. That's the one we want. 
+                    float enemy1Distance = Vector3.Distance(actingAlly.transform.position, enemy.gameObject.transform.position);
+                    float enemy2Distance = Vector3.Distance(actingAlly.transform.position, CompareEnemy.gameObject.transform.position);
+
+                    Debug.Log($"Compare enemy {CompareEnemy.gameObject.name} is {enemy2Distance} units away");
+                    Debug.Log($"Regular enemy {enemy.gameObject.name} is {enemy1Distance} units away");
+
+                    if (enemy2Distance < enemy1Distance)
+                    {
+                        enemy = CompareEnemy;
+                        CompareEnemy = null;
+                    }
+                }
             }
-            
+
         }
+
+        Debug.Log(enemy ? enemy.gameObject.name : "Nothing found" );
+        return enemy;
     }
 
     //under construction
@@ -107,81 +173,121 @@ public class Combat : MonoBehaviour
      * the atttack button needs to get the main script from the enemy EG mineral dragon() firecactus() ....
      * then attacks the enemy and the enemy HP canvas (the red bar) should drop
      */
-    public void AttackBTN () {
-        if ( CharacterWithPriority != -1 ) {
-            if ( CharacterWithPriority == 4 ) {
-                for ( int i = 0; i < allies.Length; i++ ) {
-                    for ( int j = 0; j < enemies.Length; j++ ) {
-                        float dist = Vector3.Distance ( allies [ i ].transform.position, enemies [ j ].transform.position );
-                        if ( dist <= 2.5) {
-                            FindEnemies ();
-                            //Debug.Log ( allies [ i ].ToString () + " isattacking = " + enemies [ j ].ToString ());
-                            //Characters [ i ].hasAttacked = true;
-                            //ally.Attack ();
-                            //ally.CanAct = false;
-                            //ally.AttackBar = 0;
-                            //ally.PassTurn ();
-                            //ally.DisablePlayerControl ();
-                            //Characters [ i ].hasAttacked = false;
-                            //enemy = null;
-                            //return;
-                            
-                        }
-                    }
-                }
-            } else if ( CharacterWithPriority == 5 ) {
-                for ( int i = 0; i < allies.Length; i++ ) {
-                    for ( int j = 0; j < enemies.Length; j++ ) {
-                        float dist = Vector3.Distance ( allies [ i ].transform.position, enemies [ j ].transform.position );
-                        if ( dist <= 2.5 ) {
-                           
-                            //Debug.Log ( allies[i].ToString() + " is attacking = " + enemies [ j ].ToString () );
-                            //Characters [ i ].hasAttacked = true;
-                            //ally.Attack ();
-                            //ally.CanAct = false;
-                            //ally.AttackBar = 0;
-                            //ally.PassTurn ();
-                            //ally.DisablePlayerControl ();
-                            //Characters [ i ].hasAttacked = false;
-                            //return;
-                        }
-                    }
-                }
+    public void AttackBTN()
+    {
+
+        Debug.Log("Attacking");
+        if (CharacterWithPriority != -1)
+        {
+
+            if (actingAlly.CanAct)
+            {
+                FindEnemies();
+                //Characters [ i ].hasAttacked = true;
+                //ally.Attack ();
+                //ally.CanAct = false;
+                //ally.AttackBar = 0;
+                //ally.PassTurn ();
+                //ally.DisablePlayerControl ();
+                //Characters [ i ].hasAttacked = false;
+                //enemy = null;
+                //return;
             }
+
+            //None of this is nessecary
+            //if (CharacterWithPriority == 4)
+            //{
+            //    for (int i = 0; i < allies.Count; i++)
+            //    {
+            //        for (int j = 0; j < enemies.Count; j++)
+            //        {
+            //            float dist = Vector3.Distance(allies[i].transform.position, enemies[j].transform.position);
+            //            if (dist <= 2.5f)
+            //            {
+            //                FindEnemies();
+            //                //Debug.Log ( allies [ i ].ToString () + " isattacking = " + enemies [ j ].ToString ());
+            //                //Characters [ i ].hasAttacked = true;
+            //                //ally.Attack ();
+            //                //ally.CanAct = false;
+            //                //ally.AttackBar = 0;
+            //                //ally.PassTurn ();
+            //                //ally.DisablePlayerControl ();
+            //                //Characters [ i ].hasAttacked = false;
+            //                //enemy = null;
+            //                //return;
+
+            //            }
+            //        }
+            //    }
+            //}
+            //else if (CharacterWithPriority == 5)
+            //{
+            //    for (int i = 0; i < allies.Count; i++)
+            //    {
+            //        for (int j = 0; j < enemies.Count; j++)
+            //        {
+            //            float dist = Vector3.Distance(allies[i].transform.position, enemies[j].transform.position);
+            //            if (dist <= 2.5)
+            //            {
+
+            //                //Debug.Log ( allies[i].ToString() + " is attacking = " + enemies [ j ].ToString () );
+            //                //Characters [ i ].hasAttacked = true;
+            //                //ally.Attack ();
+            //                //ally.CanAct = false;
+            //                //ally.AttackBar = 0;
+            //                //ally.PassTurn ();
+            //                //ally.DisablePlayerControl ();
+            //                //Characters [ i ].hasAttacked = false;
+            //                //return;
+            //            }
+            //        }
+            //    }
+            //}
         }
     }
 
-    public void Run () {
-        if ( CharacterWithPriority != -1 ) {
-            rng = Random.Range ( 1, 101 );
-            Debug.Log ( rng );
+    public void Run()
+    {
+        if (CharacterWithPriority != -1)
+        {
+            rng = Random.Range(1, 101);
+            Debug.Log(rng);
             //shiro is faster so higher chance of escape
-            if ( CharacterWithPriority == 4 ) {
-              if(rng >35 ) {
-                    ally.CanAct = false;
-                    ally.AttackBar = 0;
-                    ally.PassTurn ();
-                    ally.DisablePlayerControl ();
-                    Application.LoadLevel ( "OverworldTest" );
-                } else if (rng <35) {
-                    ally.CanAct = false;
-                    ally.AttackBar = 0;
-                    ally.PassTurn ();
-                    ally.DisablePlayerControl ();
+            if (CharacterWithPriority == 4)
+            {
+                if (rng > 35)
+                {
+                    actingAlly.CanAct = false;
+                    actingAlly.AttackBar = 0;
+                    actingAlly.PassTurn();
+                    actingAlly.DisablePlayerControl();
+                    Application.LoadLevel("OverworldTest");
+                }
+                else if (rng < 35)
+                {
+                    actingAlly.CanAct = false;
+                    actingAlly.AttackBar = 0;
+                    actingAlly.PassTurn();
+                    actingAlly.DisablePlayerControl();
                     return;
                 }
-            } else if ( CharacterWithPriority == 5 ) {
-                if ( rng >60 ) {
-                    ally.CanAct = false;
-                    ally.AttackBar = 0;
-                    ally.PassTurn ();
-                    ally.DisablePlayerControl ();
-                    Application.LoadLevel ( "OverworldTest" );
-                } else if (rng <60){
-                    ally.CanAct = false;
-                    ally.AttackBar = 0;
-                    ally.PassTurn ();
-                    ally.DisablePlayerControl ();
+            }
+            else if (CharacterWithPriority == 5)
+            {
+                if (rng > 60)
+                {
+                    actingAlly.CanAct = false;
+                    actingAlly.AttackBar = 0;
+                    actingAlly.PassTurn();
+                    actingAlly.DisablePlayerControl();
+                    Application.LoadLevel("OverworldTest");
+                }
+                else if (rng < 60)
+                {
+                    actingAlly.CanAct = false;
+                    actingAlly.AttackBar = 0;
+                    actingAlly.PassTurn();
+                    actingAlly.DisablePlayerControl();
                     return;
                 }
             }
@@ -190,11 +296,16 @@ public class Combat : MonoBehaviour
     /*
      * the magic button for now should attack using magic and stuff i dont think we have a mana bar set up
      */
-    public void MagicAttack () {
-        if ( CharacterWithPriority != -1 ) {
-            if ( CharacterWithPriority == 4 ) {
+    public void MagicAttack()
+    {
+        if (CharacterWithPriority != -1)
+        {
+            if (CharacterWithPriority == 4)
+            {
                 // attack with magic if enemy is in the movment grid?
-            } else if ( CharacterWithPriority == 5 ) {
+            }
+            else if (CharacterWithPriority == 5)
+            {
                 //^
             }
         }
@@ -212,17 +323,17 @@ public class Combat : MonoBehaviour
             switch (Random.Range(0, 3))
             {
                 case 0:
-                    enemyobject = Instantiate ( Resources.Load ( "SpiritBird" ), enemySpawnPoints [ i ].transform ) as GameObject;
+                    enemyobject = Instantiate(Resources.Load("SpiritBird"), enemySpawnPoints[i].transform) as GameObject;
                     enemy = enemyobject.AddComponent<SpiritBird>();
                     enemy.visual = enemyobject;
                     break;
                 case 1:
-                    enemyobject = Instantiate ( Resources.Load ( "MineralDragon" ), enemySpawnPoints [ i ].transform ) as GameObject;
+                    enemyobject = Instantiate(Resources.Load("MineralDragon"), enemySpawnPoints[i].transform) as GameObject;
                     enemy = enemyobject.AddComponent<MineralDragon>();
                     enemy.visual = enemyobject;
                     break;
                 case 2:
-                    enemyobject = Instantiate ( Resources.Load ( "FireCactus" ), enemySpawnPoints [ i ].transform ) as GameObject;
+                    enemyobject = Instantiate(Resources.Load("FireCactus"), enemySpawnPoints[i].transform) as GameObject;
                     enemy = enemyobject.AddComponent<FireCactus>();
                     enemy.visual = enemyobject;
                     break;
@@ -239,10 +350,10 @@ public class Combat : MonoBehaviour
     }
     void SpawnBoss()
     {
-        var bossSpawn = GameObject.FindGameObjectWithTag ( "Spawn_Boss" ); //TODO: Will there only be one boss at a time?
+        var bossSpawn = GameObject.FindGameObjectWithTag("Spawn_Boss"); //TODO: Will there only be one boss at a time?
 
-        GameObject bossobj ;
-        bossobj = Instantiate ( Resources.Load ( "SquidTurtle" ), bossSpawn.transform ) as GameObject;
+        GameObject bossobj;
+        bossobj = Instantiate(Resources.Load("SquidTurtle"), bossSpawn.transform) as GameObject;
         var boss = bossobj.AddComponent<SquidTurtle>();
         boss.visual = bossobj;
 
@@ -252,16 +363,16 @@ public class Combat : MonoBehaviour
     }
     void SpawnCharacters()
     {
-        var allySpawnPoints = GameObject.FindGameObjectsWithTag ( "Spawn_Ally" );
+        var allySpawnPoints = GameObject.FindGameObjectsWithTag("Spawn_Ally");
 
-        var shiroSpawnIndex = Random.Range ( 0, 2 );
+        var shiroSpawnIndex = Random.Range(0, 2);
         var binxSpawnIndex = 1 - shiroSpawnIndex;
 
         GameObject shiroobj;
         GameObject binxobj;
 
         shiroobj = Instantiate(Resources.Load("Shiro"), allySpawnPoints[shiroSpawnIndex].transform) as GameObject;
-        binxobj = Instantiate ( Resources.Load ( "Binx" ), allySpawnPoints [ binxSpawnIndex ].transform ) as GameObject;
+        binxobj = Instantiate(Resources.Load("Binx"), allySpawnPoints[binxSpawnIndex].transform) as GameObject;
 
         var shiro = shiroobj.AddComponent<Shiro>();
         var binx = binxobj.AddComponent<Binx>();
@@ -310,36 +421,40 @@ public class Combat : MonoBehaviour
                 PassPriority(i);
 
         }
-        for ( int i = 0; i < Characters.Count; i++ ) {
-            if ( Characters [ i ].CanAct ) {
-                Debug.Log ( "transform" );
-                Characters [ i ].MenuDisplay ();
+        for (int i = 0; i < Characters.Count; i++)
+        {
+            if (Characters[i].CanAct)
+            {
+                Debug.Log("transform");
+                Characters[i].MenuDisplay();
             }
         }
 
-        
+
         //TODO: Check if all characters in one side are dead.
         //TODO: Acutally, maybe just check when a character dies
     }
-    
+
     void PassPriority(int id)
     {
         CharacterWithPriority = id;
         Characters[CharacterWithPriority].ReceivPriority();
     }
 
-    
+
 
     private void NotifyAttack(string charName)
     {
         Debug.Log(charName + " Is able to attack at time " + Time.time);
     }
 
-    void CombatEnter () {
+    void CombatEnter()
+    {
         //TODO: implement combat enter condition
     }
 
-    void CombatExit () {
+    void CombatExit()
+    {
         //TODO: implement combat end condition
     }
 }
