05e68d4ef84edb4e4a3c04719e6d414ede46a8fe
ricky richards <roosterrich30@gmail.com>
2019-11-05 11:54:39 +0000

    here is the dialog with follow player scripts

---
+++ Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour {
+
+	public TextMeshProUGUI textdsiplay;
+	public string[] sentence;
+	public int index;
+	public float typingspeed;
+	public bool Istalking = false;
+	public bool hasFinished;
+	public bool doneTalking;
+	void Start(){
+		textdsiplay.text = "";
+	}
+	void Update(){
+		/*
+		do distance check here then run scripts
+		 */
+		//checks if its false then activates the sentences
+		if(Istalking == false){
+			if (Input.GetKeyDown(KeyCode.M)){
+				Istalking = true;
+				hasFinished = false;
+				doneTalking = false;
+				StartCoroutine(Type());
+			}
+		}
+		//puts up the next sentence
+		if (Istalking){
+			if(!doneTalking){
+				if(Input.GetKeyDown(KeyCode.X)){
+					textdsiplay.text = "";
+					index++;
+					StartCoroutine(Type());
+				}
+			}
+		}
+		if (doneTalking){
+			textdsiplay.text = "";
+			StopAllCoroutines();
+		}
+	}
+	IEnumerator Type(){
+		if(index == sentence.Length){
+			index = 0;
+			Istalking = false;
+			doneTalking = true;
+		}	
+		if(sentence.Length != index){	
+		foreach (char letter in sentence[index].ToCharArray()){
+			textdsiplay.text += letter; 	
+			yield return new WaitForSeconds(typingspeed);
+			//wip
+			//hasFinished = true;
+			}
+		}
+	}
+}
--- Assets/Scripts/Follow Player/FollowPlayer.cs
+++ Assets/Scripts/Follow Player/FollowPlayer.cs
@@ -9,7 +9,11 @@ public class FollowPlayer : MonoBehaviour {
 	public float rotationSpeed = 3;
 	public Vector3 dir;
     public GameObject [] OtherCom;
-    public Transform [] OtherCompT;
+	public GameObject comp1;
+	public GameObject comp2;
+	public GameObject marker;
+	public GameObject comp3;
+	public GameObject comp4;
 	public Transform myTransform ; 
 	public float distance;
     public float Dist;
@@ -22,58 +26,75 @@ public class FollowPlayer : MonoBehaviour {
 		myTransform = transform;
         target = GameObject.FindGameObjectWithTag ( "Player" ).transform;
         OtherCom = GameObject.FindGameObjectsWithTag ( "Comp" );
-        
+		for ( int i = 0; i < OtherCom.Length; i++ ) {
+			Debug.Log(OtherCom.Length);
+			if(i == 1){
+				Debug.Log("found 1 or 2");
+				comp1 = OtherCom[0];
+			}
+			if(i == 2){
+				comp2 = OtherCom[1];
+				comp1 = OtherCom[0];
+			}
+			if(i == 3){
+				comp3 = OtherCom[2];
+				comp2 = OtherCom[1];
+				comp1 = OtherCom[0];
+			}
+			if (i == 4){
+				comp4 = OtherCom[3];
+				comp3 = OtherCom[2];
+				comp2 = OtherCom[1];
+				comp1 = OtherCom[0];
+			}
+			
+		}
+		marker = GameObject.Find("marker");
+
 	}
 
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
+		
+		
+		
 		// this.transform.rotation = Quaternion.LookRotation(Vector3.zero);
 	}
 
 	void Update () {
 		distance = Vector3.Distance (myTransform.position, target.position);
-        for ( int i = 0; i < OtherCom.Length; i++ ) {
-			Debug.Log("i have found companions");
+        
+		for ( int i = 0; i < OtherCom.Length; i++ ) {
+			
+			Debug.Log("i have found companions: "+ OtherCom.Length);
 			if (PlayerCloseEnough){
+				Debug.Log("companions are close");
 				//move compnaions x distance away from eachother
-				Dist = Vector3.Distance(this.transform.position, new Vector3(OtherCom[i].transform.position.x,transform.position.y,transform.position.z));
-				if (Dist < stop){
+				Dist = Vector3.Distance(new Vector3(OtherCom[i].transform.position.x,OtherCom[i].transform.position.y,OtherCom[i].transform.position.z), new Vector3(OtherCom[i].transform.position.x,OtherCom[i].transform.position.y,OtherCom[i].transform.position.z));
+				if (Dist < CStop){
 					this.transform.rotation = Quaternion.Slerp(this.transform.rotation,	Quaternion.LookRotation(dir),rotationSpeed *Time.deltaTime);					
-					//moveSpeed = 3;
+					// Debug.DrawLine(this.transform.position,OtherCom[i].transform.position,Color.red);
+					Debug.Log("plz move away");
+					moveSpeed = 1.5f;
+					var dist2 = Vector3.Distance(comp1.transform.position,marker.transform.position);
+					if(dist2>CStop){
+						comp1.transform.position += comp1.transform.right * moveSpeed * Time.deltaTime;
+					}
+					
 				}
-				else if (Dist> stop){
-					this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(target.transform.position),rotationSpeed*Time.deltaTime);
-			//		moveSpeed = 0;
+				else {	
+					moveSpeed = 0;
 				}
-
 			}
+		}
 
-            // Dist = Vector3.Distance (myTransform.transform.position, OtherCom[i].gameObject.transform.position);
-            // if ( Vector3.Distance(myTransform.transform.position,OtherCom[i].transform.position) > Dist ) {
-            //     OtherCom[i].gameObject.transform.position = new Vector3(3,0,2);
-			// 	Debug.Log("not moving");
-            //     moveSpeed = 0;
-            // } else if ( Vector3.Distance ( OtherCom [ i ].transform.position, myTransform.position ) < Dist ) {
-            //     CompCloseEnough = false;
-			// 	Debug.Log("moving");
-            //     moveSpeed = 3;
-            // }
-			// Dist2 = Vector3.Distance(OtherCom[i].transform.position, OtherCom[i].transform.position);
-			// if(Vector3.Magnitude(OtherCom[i].transform.position)<Dist2) {
-			// 	moveSpeed = -3;
-            // }
-			// else {
-			// 	moveSpeed=3;
-			// }
-           
-        }
-        //^^ myTransform.position, OtherCom [ i ].gameObject.transform.position
+         //^^ myTransform.position, OtherCom [ i ].gameObject.transform.position
         
 		if (distance <= stop) {
 			//moveSpeed = 0;  
 			PlayerCloseEnough = true;
 		} else {
-			moveSpeed = 3;
+			moveSpeed = 1.5f;
 			PlayerCloseEnough = false;
 			var lookDir = target.position - myTransform.position;
 			lookDir.y = 0; // zero the height difference
